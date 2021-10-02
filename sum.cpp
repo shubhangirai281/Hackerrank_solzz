718b299c760fb9bc35d82542e4e9ff557e9d58cc
shubhangirai281 <81921977+shubhangirai281@users.noreply.github.com>
2021-10-02 23:58:24 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int simpleArraySum(vector<int> ar) {
+    
+
+}
+
+int main()
+{
+ int i, ar[i] ;
+  for ( i= 0; i<= 1000; i++){
+      
+  } 
+ 
+    return 0;
+}
+
