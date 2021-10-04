c6d06f3c862cc6a3f78f8f7927a600fb4fd676fe
shubhangirai281 <81921977+shubhangirai281@users.noreply.github.com>
2021-10-05 00:06:03 +0530

    Add files via upload

--- sumofarr.cpp
+++ sumofarr.cpp
@@ -1,17 +1,29 @@
+
+/*
+
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-    int number_of_elements;
-    cin >> number_of_elements;
-    vector <int> array(number_of_elements);
-    int sum_of_array = 0;
-    
-    for(int i = 0; i < number_of_elements; i++){
-       cin >> array[i];
-       sum_of_array += array[i];
-    }
-    
-    cout << sum_of_array;
+
+
+int main()
+{
+    int m, s=0, ar[1000];
+    cin>>m;
+    for(int i=0; i<m; i++){
+        cin >>ar[i];
+        s= s + ar[i];
+    } 
+cout<<s;
     return 0;
 }
+
