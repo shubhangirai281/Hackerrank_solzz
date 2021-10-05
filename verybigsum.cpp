37ed509695751a1b644466fef5baf36fe00ec834
shubhangirai281 <81921977+shubhangirai281@users.noreply.github.com>
2021-10-06 03:36:16 +0530

    Add files via upload

---
+++ verybigsum.cpp
@@ -0,0 +1,33 @@
+
+/*
+In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+Function Description
+
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+aVeryBigSum has the following parameter(s):
+
+int ar[n]: an array of integers .
+Return
+ */
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+int main()
+{
+int long s=0, m, ar[1000];
+cin>>m;
+for(int i=0; i<m; i++){
+    cin>>ar[i];
+    s=s+ar[i];
+}
+cout<<s;
+    return 0;
+}
